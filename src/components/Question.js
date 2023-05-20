7f82ada32d48f2abc480f7275d90d236bc1ee66f
DennisMatenge <mateshdennis@gmail.com>
2023-05-20 19:20:52 +0300

    first commit

---
+++ src/components/Question.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+function Question({ question, onAnswered }) {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  // add useEffect code
+  // useEffect to set timeout
+  useEffect(() => {
+    console.log("effect rady")
+    setTimeout(() => setTimeRemaining(timeRemaining - 1),1000)
+  })
+
+  // useEffect to cleanup
+  useEffect(() => {
+    console.log("effect noice")       
+    
+  })
+  
+
+  function handleAnswer(isCorrect) {
+    setTimeRemaining(10);
+    onAnswered(isCorrect);
+  }
+
+  const { id, prompt, answers, correctIndex } = question;
+
+  return (
+    <>
+      <h1>Question {id}</h1>
+      <h3>{prompt}</h3>
+      {answers.map((answer, index) => {
+        const isCorrect = index === correctIndex;
+        return (
+          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+            {answer}
+          </button>
+        );
+      })}
+      <h5>{timeRemaining} seconds remaining</h5>
+    </>
+  );
+}
+
+export default Question;
